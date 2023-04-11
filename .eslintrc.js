aec3a4addfec6ddb5e9c3bc0e047915eb40a8d11
Santiago Arias <santiago.arias@rootstrap.com>
2023-04-11 09:12:17 -0500

    add code analysis tools (including sonarjs rules)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,67 @@
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    "plugin:sonarjs/recommended"
+  ],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
     jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }
+    },
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    'no-useless-constructor': 'off',
+    'import/prefer-default-export': 'off',
+    "sort-imports": ["error", {
+      "ignoreCase": false,
+      "ignoreDeclarationSort": true,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["none", "all", "single", "multiple"]
+    }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "ts": "never",
+      }
+    ],
+    "class-methods-use-this": "off",
+    "import/no-extraneous-dependencies":[
+      "error",
+      {
+         "devDependencies":[
+            "**/*.spec.ts",
+            "test/**/*",
+         ]
+      }
+    ]
   },
 };

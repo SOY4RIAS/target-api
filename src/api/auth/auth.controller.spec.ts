88794c6b92139637b18eefb813003c4f5a69a78a
Santiago Arias <santiago.arias@rootstrap.com>
2023-05-31 03:31:03 -0500

    fix tests

--- src/api/auth/auth.controller.spec.ts
+++ src/api/auth/auth.controller.spec.ts
@@ -1,13 +1,23 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 
 import { AuthController } from './auth.controller';
+import { UserService } from '../user/user.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findUserByEmail: jest.fn(),
+            createUser: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/api/user/user.service.spec.ts
+++ src/api/user/user.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(User),
+          useFactory: jest.fn(() => ({
+            findOneBy: jest.fn(),
+          })),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);

ac0655085982afd36987ff83c54627782933cd22
Santiago Arias <santiago.arias@rootstrap.com>
2023-07-16 23:53:51 -0500

    feat(Auth): add global auth

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class ApiModule {}
--- src/api/auth/guards/jwt-auth.guard.ts
+++ src/api/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+import { SKIP_AUTH_KEY } from '@common/guards/skip-auth.guard';
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const skipAuth = this.reflector.getAllAndOverride<boolean>(SKIP_AUTH_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (skipAuth) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}

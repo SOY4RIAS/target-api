aefac3477686321ab13e82a1143ecf49652528aa
Santiago Arias <santiago.arias@rootstrap.com>
2023-08-01 14:33:36 -0500

    refactor: update return types

--- src/api/topics/topics.controller.ts
+++ src/api/topics/topics.controller.ts
@@ -10,6 +10,7 @@ import {
 
 import { CreateTopicDto } from '@api/topics/dto/create-topic.dto';
 import { UpdateTopicDto } from '@api/topics/dto/update-topic.dto';
+import { TopicEntity } from '@api/topics/topic.entity';
 import { TopicsService } from '@api/topics/topics.service';
 
 @Controller('topics')
@@ -22,22 +23,22 @@ export class TopicsController {
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<TopicEntity[]> {
     return this.topicsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<TopicEntity | null> {
     return this.topicsService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTopicDto: UpdateTopicDto) {
-    return this.topicsService.update(+id, updateTopicDto);
+    this.topicsService.update(+id, updateTopicDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.topicsService.remove(+id);
+    this.topicsService.remove(+id);
   }
 }
